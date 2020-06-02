a13c18d90e6e03ddeec367726f7ee64aefe63193
[williamsconcepts] <[chidiekwebelam@gmail.com]>
2020-06-02 10:28:17 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com';
+export const key = '462b1cc8d4f2730081462fbc65136320';
+
---
+++ src/js/models/Search.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        // const proxy = 'https://cors-anywhere.herokuapp.com';
+        // const key = '462b1cc8d4f2730081462fbc65136320';
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
+
+
+
+
